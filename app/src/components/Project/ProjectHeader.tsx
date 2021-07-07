3245a54a7d4b9afdf116acf830191127dec6a959
Emily Yang <zyan225@aucklanduni.ac.nz>
2021-07-07 16:13:45 +1200

    [CDL-8] setup the tap in the project page

---
+++ app/src/components/Project/ProjectHeader.tsx
@@ -0,0 +1,50 @@
+import { IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
+import { ellipse, square, triangle } from 'ionicons/icons';
+import React, { useEffect } from 'react';
+import { Route, useRouteMatch, Switch, useParams } from 'react-router';
+import ProjectInsight from './ProjectInsight';
+import ProjectLabelling from './ProjectLabelling';
+import ProjectSettings from './ProjectSettings';
+
+interface ProjectHeaderProps {
+
+}
+
+const ProjectHeader: React.FC<ProjectHeaderProps> = (props: ProjectHeaderProps) => {
+
+    const { name } = useParams<{ name: string }>();
+
+    return (
+        <div>
+            Hello this is project header
+            <IonTabBar>
+                <IonTabButton tab="tab1" href={`/project/${name}/labelling`}>
+                    <IonIcon icon={triangle} />
+                    <IonLabel>Labelling</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="tab2" href={`/project/${name}/insight`}>
+                    <IonIcon icon={ellipse} />
+                    <IonLabel>Insight</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="tab3" href={`/project/${name}/setting`}>
+                    <IonIcon icon={square} />
+                    <IonLabel>Settings</IonLabel>
+                </IonTabButton>
+            </IonTabBar>
+            
+            <Switch>
+                <Route exact path={`/project/${name}/labelling`}>
+                    <ProjectLabelling />
+                </Route>
+                <Route exact path={`/project/${name}/insight`}>
+                    <ProjectInsight />
+                </Route>
+                <Route exact path={`/project/${name}/setting`}>
+                    <ProjectSettings />
+                </Route>
+            </Switch>
+        </div>
+    );
+};
+
+export default ProjectHeader;
